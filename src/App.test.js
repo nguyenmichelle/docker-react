caa127af92a96b2837b3827b37af93a43ae51927
Michelle Nguyen <michellenguyen@Michelles-MacBook-Pro.local>
2021-11-11 18:58:47 -0700

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('4 is answer', () => {
+  const dom = render(<App />);
+  expect(dom.container.querySelector('#answer')).toH('4')
+});
+test('6 is answer', () => {
+  const dom = render(<App />);
+  expect(dom.container.querySelector('#answer')).toHaveTextContent('6')
 });
